5c2569a7c3aa97c4cce274a378b236bcd827038b
diogo735 <128258066+diogo735@users.noreply.github.com>
2024-10-16 15:19:00 +0100

    Primeiro

---
+++ app_escola/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppEscolaConfig(AppConfig):
+    name = 'app_escola'
---
+++ app_escola/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.db import connection 
+
+def home(request):
+    try:
+        # Tenta fazer uma consulta simples ao banco de dados
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")  # Consulta simples para verificar a conexão
+        status = "OK"
+    except Exception as e:
+        # Se houver qualquer erro na conexão, define como "Not OK"
+        status = "Not OK"
+
+    # Renderiza o template home.html com o status da conexão
+    return render(request, 'home.html', {'db_status': status})
